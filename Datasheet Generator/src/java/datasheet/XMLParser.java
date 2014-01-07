b50fbe2e0bb392fe5aac1b12478f86f502f32231
Pooja <Pooja@155.41.25.152>
2014-01-07 17:23:51 -0500

    took out commented out communication code; only parsing code and main

--- Datasheet Generator/src/java/datasheet/XMLParser.java
+++ Datasheet Generator/src/java/datasheet/XMLParser.java
@@ -10,15 +10,6 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.parser.Parser;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 
 /**
  *
@@ -156,84 +147,4 @@ public class XMLParser {
         return partsInfoJSON; //return partsInfoJSON JSON object
     }
     
-//    public class CommunicationExampleServlet extends HttpServlet {
-//
-//    /**
-//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-//     * methods.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//        
-//        protected void processPostRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("application/json");
-//        String data = request.getParameter("data");
-//        //print out data posted to server
-//        System.out.println(data.toString());
-//        }
-//
-//        protected void processGetRequest(HttpServletRequest request, HttpServletResponse response, JSONArray partsInfoJSON)
-//            throws ServletException, IOException, JSONException {
-//        response.setContentType("application/json");
-//        PrintWriter out = response.getWriter();
-//        
-//        //create a new json object
-//        //JSONObject toReturn = new JSONObject();
-//        
-//        //add new key value pair to json object        
-//        //toReturn.put("message", "received from the server");
-//            
-//        //return the json object as a string
-//        //out.write(toReturn.toString());
-//        out.write(partsInfoJSON.toString());
-//        
-//        }
-//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-//    /**
-//     * Handles the HTTP <code>GET</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response, JSONArray partsInfoJSON)
-//            throws ServletException, IOException {
-//        try {
-//            processGetRequest(request, response, partsInfoJSON);
-//        } catch (JSONException ex) {
-//            Logger.getLogger(CommunicationExampleServlet.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processPostRequest(request, response);
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
-//    
-//    }
 }
